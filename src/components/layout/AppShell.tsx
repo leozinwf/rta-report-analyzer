fec96273675aefb3ceb0e705e404900e9c8557ef
Leonardo <leoomorais@hotmail.com>
2026-09-15 09:21:53 -0300

    feat: expose ChatGPT analysis package in app

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { ChatGptExport } from "../ai/ChatGptExport";
 import { GlobalFilters } from "../filters/GlobalFilters";
 import { Sidebar } from "./Sidebar";
 import { TopBar } from "./TopBar";
@@ -10,6 +11,7 @@ export function AppShell() {
       <div className="flex min-w-0 flex-1 flex-col">
         <TopBar />
         <GlobalFilters />
+        <ChatGptExport />
         <main className="flex-1 overflow-auto p-6">
           <Outlet />
         </main>
